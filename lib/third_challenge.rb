e810ca47d4d23b6ce5e639e65570b62c6cbd958d
Joseph Moloughney <jmoloughney25@gmail.com>
2017-11-06 16:53:37 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,36 +1,36 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
+   montague: {
+      patriarch: {
+        name: "Lord Montague",
+        age: "53"
         },
-      :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
+      matriarch: {
+        name: "Lady Montague",
+        age: "54"
         },
-      :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
+      hero: {
+        name: "Romeo",
+        age: "15",
+        status: "alive"
         },
-      :hero_friends => []
+      hero_friends: []
    },
-   :capulet => {
-      :patriarch => {
+   capulet:  {
+      patriarch: {
         name: "Lord Capulet",
         age: "50"
         },
-      :matriarch => {
+      matriarch: {
         name: "Lady Capulet",
         age: "51"
         },
-      :heroine => {
+      heroine: {
         name: "Juliet",
         age: "15",
         status: "alive"
         },
-      :heroine_friends => []
+      heroine_friends: []
    }
   }
 
